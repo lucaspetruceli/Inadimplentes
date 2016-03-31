0d517f994712cf956f97f39d6c95e3a02f602c9d
Victor Oliveira <Victor Oliveira>
2016-03-31 13:47:48 -0300

    Adição dos Headers no app_config

--- app_config.js
+++ app_config.js
@@ -6,6 +6,16 @@ var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 1337;
 
+var allowCors = function(req, res, next){
+
+	res.header('Acess-Control-Allow-Origin', '*');
+	res.header('Acess-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+	res.header('Acess-Control-Allow-Headers', 'Content-Type');
+	res.header('Acess-Control-Allow-Credentials', 'true');
+
+	next();
+
+}
 app.listen(port);
 
 app.use(bodyParser.json());
