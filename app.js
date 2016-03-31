cd21a58f7871657126c9661427b103a737ae574d
lucaspetruceli <lucaspetruceli@hotmail.com>
2016-03-31 01:12:05 -0300

    Alterações no App.js em relação as requisições

--- app.js
+++ app.js
@@ -10,3 +10,29 @@ app.get('/', function(req, res){
         
         res.end('Servidor ON!');
 });
+
+app.get('/users', function(req, res){
+        
+    
+    
+});
+
+app.get('/users/:id', function(req, res){
+        
+        
+});
+
+app.post('/users', function(req, res){
+        
+    res.end('post users')
+});
+
+app.put('/users', function(req, res){
+        
+    res.end('put user')
+});
+
+app.delete('/users/:id', function(req, res){
+        
+    res.end('delete user')
+});
